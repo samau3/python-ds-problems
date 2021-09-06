d135f514c3709cdf4f09c1ddd986801cf85ef285
samau3 <samau415@gmail.com>
2021-09-06 16:17:11 -0700

    completed problems 1-5

--- 02_weekday_name.py
+++ 02_weekday_name.py
@@ -12,3 +12,11 @@ def weekday_name(day_of_week):
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
+    weekdays = {1: "Sunday", 2: "Monday", 3: "Tuesday",
+                4: "Wednesday", 5: "Thursday", 6: "Friday",
+                7: "Saturday"}
+    
+    if day_of_week <= 7 and day_of_week >= 1:
+        return weekdays[day_of_week]
+    else:
+        return None
